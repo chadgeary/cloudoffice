8499875e6faef19743bd897796f739f32f1f22ce
chadgeary <chad.geary@gmail.com>
2021-01-12 01:24:57 +0000

    notes about destroying objects in terraform output for do

--- do/do-output.tf
+++ do/do-output.tf
@@ -24,5 +24,18 @@ output "cloudblock-output" {
   # Re-apply Ansible playbook with custom variables
   cd playbooks/
   sudo ansible-playbook nextcloud_do.yml --extra-vars 'docker_network=${var.docker_network} docker_gw=${var.docker_gw} docker_nextcloud=${var.docker_nextcloud} docker_db=${var.docker_db} docker_webproxy=${var.docker_webproxy} docker_onlyoffice=${var.docker_onlyoffice} nc_prefix=${var.nc_prefix} nc_suffix=${random_string.nc-random.result} do_storageaccessid=${var.do_storageaccessid} do_storagesecretkey=${var.do_storagesecretkey} do_region=${var.do_region} instance_public_ip=${digitalocean_floating_ip.nc-ip.ip_address} web_port=${var.web_port} oo_port=${var.oo_port} project_directory=${var.project_directory} admin_password=${var.admin_password} db_password=${var.db_password} oo_password=${var.oo_password}'
+  
+  ## Destroying ##
+  
+  # Before terraform destroy, delete all objects from buckets using the aws CLI - this action is irreversible.
+  # Install awscli via pip3
+  sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt-get -q -y install python3-pip
+  pip3 install --user --upgrade awscli
+  # Set credentials
+  aws configure set aws_access_key_id ${var.do_storageaccessid}
+  aws configure set aws_access_secret_key ${var.do_storagesecretkey}
+  # Remove objects
+  aws s3 rm --recursive s3://${var.nc_prefix}-bucket-${random_string.nc-random.result}/ --endpoint https://${var.do_region}.digitaloceanspaces.com
+  aws s3 rm --recursive s3://${var.nc_prefix}-bucket-data-${random_string.nc-random.result}/ --endpoint https://${var.do_region}.digitaloceanspaces.com
   OUTPUT
 }
