0b867fe4f480f6ac3c63588260393be440669362
chadgeary <chad.geary@gmail.com>
2021-01-23 18:35:41 +0000

    do output reference template file

--- do/do-output.tf
+++ do/do-output.tf
@@ -1,3 +1,39 @@
+resource "local_file" "do_setup" {
+  filename                          = "${var.nc_prefix}-setup-${random_string.nc-random.result}.sh"
+  content                           = <<FILECONTENT
+# Create cloudoffice-ansible-state script
+sudo tee /opt/cloudoffice-ansible-state.sh << EOM
+#!/bin/bash
+# Update package list
+apt-get update
+# Install pip3 and git
+DEBIAN_FRONTEND=noninteractive apt-get -y install python3-pip git
+# Install ansible
+pip3 install --upgrade ansible
+# Make the project directory
+mkdir -p /opt/git/cloudoffice
+# Clone project into project directory
+git clone ${var.project_url} /opt/git/cloudoffice
+# Change to directory
+cd /opt/git/cloudoffice
+# Ensure up-to-date
+git pull
+# Change to playbooks directory
+cd playbooks/
+# Execute playbook
+ansible-playbook cloudoffice_do.yml --extra-vars 'docker_network=${var.docker_network} docker_gw=${var.docker_gw} docker_nextcloud=${var.docker_nextcloud} docker_db=${var.docker_db} docker_webproxy=${var.docker_webproxy} docker_onlyoffice=${var.docker_onlyoffice} nc_prefix=${var.nc_prefix} nc_suffix=${random_string.nc-random.result} admin_password=${var.admin_password} db_password=${var.db_password} oo_password=${var.oo_password} instance_public_ip=${digitalocean_floating_ip.nc-ip.ip_address} do_region=${var.do_region} do_storageaccessid=${var.do_storageaccessid} do_storagesecretkey=${var.do_storagesecretkey} web_port=${var.web_port} oo_port=${var.oo_port} project_directory=${var.project_directory}' >> /var/log/cloudoffice.log
+EOM
+
+# Start / Enable cloudoffice-ansible-state
+sudo chown root:root /opt/cloudoffice-ansible-state.sh
+sudo chmod 500 /opt/cloudoffice-ansible-state.sh
+sudo systemctl daemon-reload
+sudo systemctl start cloudoffice-ansible-state.service
+sudo systemctl enable cloudoffice-ansible-state.timer
+sudo systemctl enable cloudoffice-ansible-state.service
+  FILECONTENT
+}
+
 output "cloudblock-output" {
   value = <<OUTPUT
 
@@ -14,37 +50,8 @@ output "cloudblock-output" {
   ## ##################### ##
   ## Ansible Service Setup ##
   ## ##################### ##
-  
-  # Create cloudoffice-ansible-state script
-  sudo tee /opt/cloudoffice-ansible-state.sh << EOM
-  #!/bin/bash
-  # Update package list
-  apt-get update
-  # Install pip3 and git
-  DEBIAN_FRONTEND=noninteractive apt-get -y install python3-pip git
-  # Install ansible
-  pip3 install --upgrade ansible
-  # Make the project directory
-  mkdir -p /opt/git/cloudoffice
-  # Clone project into project directory
-  git clone ${var.project_url} /opt/git/cloudoffice
-  # Change to directory
-  cd /opt/git/cloudoffice
-  # Ensure up-to-date
-  git pull
-  # Change to playbooks directory
-  cd playbooks/
-  # Execute playbook
-  ansible-playbook cloudoffice_do.yml --extra-vars 'docker_network=${var.docker_network} docker_gw=${var.docker_gw} docker_nextcloud=${var.docker_nextcloud} docker_db=${var.docker_db} docker_webproxy=${var.docker_webproxy} docker_onlyoffice=${var.docker_onlyoffice} nc_prefix=${var.nc_prefix} nc_suffix=${random_string.nc-random.result} admin_password=${var.admin_password} db_password=${var.db_password} oo_password=${var.oo_password} instance_public_ip=${digitalocean_floating_ip.nc-ip.ip_address} do_region=${var.do_region} do_storageaccessid=${var.do_storageaccessid} do_storagesecretkey=${var.do_storagesecretkey} web_port=${var.web_port} oo_port=${var.oo_port} project_directory=${var.project_directory}' >> /var/log/cloudoffice.log
-  EOM
-  
-  # Start / Enable cloudoffice-ansible-state
-  sudo chown root:root /opt/cloudoffice-ansible-state.sh
-  sudo chmod 500 /opt/cloudoffice-ansible-state.sh
-  sudo systemctl daemon-reload
-  sudo systemctl start cloudoffice-ansible-state.service
-  sudo systemctl enable cloudoffice-ansible-state.timer
-  sudo systemctl enable cloudoffice-ansible-state.service
+  scp ${var.nc_prefix}-setup-${random_string.nc-random.result}.sh ubuntu@{digitalocean_floating_ip.nc-ip.ip_address}:~/${var.nc_prefix}-setup-${random_string.nc-random.result}.sh
+  ssh ubuntu@{digitalocean_floating_ip.nc-ip.ip_address} "chmod +x ${var.nc_prefix}-setup-${random_string.nc-random.result}.sh && ${var.nc_prefix}-setup-${random_string.nc-random.result}.sh"
   
   ## ################################################ ##
   ## Update Containers and Ansible Rerun Instructions ##
