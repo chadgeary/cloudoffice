cd22d4d1805afbb32db5f4f58c9736e7878ebb2c
chadgeary <chad.geary@gmail.com>
2021-01-23 02:21:44 +0000

    do change steps to use manual ansible run

--- do/do-output.tf
+++ do/do-output.tf
@@ -4,23 +4,60 @@ output "cloudblock-output" {
   #############
   ## OUTPUTS ##
   #############
-
+  
   ## SSH ##
   ssh ubuntu@${digitalocean_floating_ip.nc-ip.ip_address}
-
+  
   ## WebUI ##
   https://${digitalocean_floating_ip.nc-ip.ip_address}:${var.web_port}/
-
-  ## Update Containers / Ansible Rerun Instructions ##
+  
+  ## ##################### ##
+  ## Ansible Service Setup ##
+  ## ##################### ##
+  
+  # Create cloudoffice-ansible-state script
+  sudo tee /opt/cloudoffice-ansible-state.sh << EOM
+  #!/bin/bash
+  # Update package list
+  apt-get update
+  # Install pip3 and git
+  DEBIAN_FRONTEND=noninteractive apt-get -y install python3-pip git
+  # Install ansible
+  pip3 install --upgrade ansible
+  # Make the project directory
+  mkdir -p /opt/git/cloudoffice
+  # Clone project into project directory
+  git clone ${var.project_url} /opt/git/cloudoffice
+  # Change to directory
+  cd /opt/git/cloudoffice
+  # Ensure up-to-date
+  git pull
+  # Change to playbooks directory
+  cd playbooks/
+  # Execute playbook
+  ansible-playbook cloudoffice_do.yml --extra-vars 'docker_network=${var.docker_network} docker_gw=${var.docker_gw} docker_nextcloud=${var.docker_nextcloud} docker_db=${var.docker_db} docker_webproxy=${var.docker_webproxy} docker_onlyoffice=${var.docker_onlyoffice} nc_prefix=${var.nc_prefix} nc_suffix=${random_string.nc-random.result} admin_password=${var.admin_password} db_password=${var.db_password} oo_password=${var.oo_password} instance_public_ip=${digitalocean_floating_ip.nc-ip.ip_address} do_region=${var.do_region} do_storageaccessid=${var.do_storageaccessid} do_storagesecretkey=${var.do_storagesecretkey} web_port=${var.web_port} oo_port=${var.oo_port} project_directory=${var.project_directory}' >> /var/log/cloudoffice.log
+  EOM
+  
+  # Start / Enable cloudoffice-ansible-state
+  sudo chown root:root /opt/cloudoffice-ansible-state.sh
+  sudo chmod 500 /opt/cloudoffice-ansible-state.sh
+  sudo systemctl daemon-reload
+  sudo systemctl start cloudoffice-ansible-state.service
+  sudo systemctl enable cloudoffice-ansible-state.timer
+  sudo systemctl enable cloudoffice-ansible-state.service
+  
+  ## ################################################ ##
+  ## Update Containers and Ansible Rerun Instructions ##
+  ## ################################################ ##
   ssh ubuntu@${digitalocean_floating_ip.nc-ip.ip_address}
-
+  
   # If updating containers, remove the old containers - this brings down the service until ansible is re-applied.
   sudo docker rm -f nextcloud_application nextcloud_database nextcloud_webproxy nextcloud_onlyoffice
-
+  
   # Update project
   cd /opt/git/nextcloud/
   sudo git pull
-
+  
   # Re-apply Ansible playbook with custom variables
   cd playbooks/
   sudo ansible-playbook nextcloud_do.yml --extra-vars 'docker_network=${var.docker_network} docker_gw=${var.docker_gw} docker_nextcloud=${var.docker_nextcloud} docker_db=${var.docker_db} docker_webproxy=${var.docker_webproxy} docker_onlyoffice=${var.docker_onlyoffice} nc_prefix=${var.nc_prefix} nc_suffix=${random_string.nc-random.result} do_storageaccessid=${var.do_storageaccessid} do_storagesecretkey=${var.do_storagesecretkey} do_region=${var.do_region} instance_public_ip=${digitalocean_floating_ip.nc-ip.ip_address} web_port=${var.web_port} oo_port=${var.oo_port} project_directory=${var.project_directory} admin_password=${var.admin_password} db_password=${var.db_password} oo_password=${var.oo_password}'
