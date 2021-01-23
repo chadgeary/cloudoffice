83c708f9a16dab9673914d4e3581bd6f13c965a6
chadgeary <chad.geary@gmail.com>
2021-01-23 20:49:57 +0000

    reapply cloudoffice via ansible state for do

--- do/do-output.tf
+++ do/do-output.tf
@@ -90,13 +90,8 @@ output "cloudblock-output" {
   # If updating containers, remove the old containers - this brings down the service until ansible is re-applied.
   sudo docker rm -f cloudoffice_nextcloud cloudoffice_database cloudoffice_webproxy cloudoffice_onlyoffice
   
-  # Update project
-  cd /opt/git/nextcloud/
-  sudo git pull
-  
-  # Re-apply Ansible playbook with custom variables
-  cd playbooks/
-  sudo ansible-playbook nextcloud_do.yml --extra-vars 'docker_network=${var.docker_network} docker_gw=${var.docker_gw} docker_nextcloud=${var.docker_nextcloud} docker_db=${var.docker_db} docker_webproxy=${var.docker_webproxy} docker_onlyoffice=${var.docker_onlyoffice} nc_prefix=${var.nc_prefix} nc_suffix=${random_string.nc-random.result} do_storageaccessid=${var.do_storageaccessid} do_storagesecretkey=${var.do_storagesecretkey} do_region=${var.do_region} instance_public_ip=${digitalocean_floating_ip.nc-ip.ip_address} web_port=${var.web_port} oo_port=${var.oo_port} project_directory=${var.project_directory} admin_password=${var.admin_password} db_password=${var.db_password} oo_password=${var.oo_password}'
+  # Re-apply Ansible playbook via systemd service
+  sudo systemctl start cloudoffice-ansible-state.service
   
   ## Destroying ##
   
