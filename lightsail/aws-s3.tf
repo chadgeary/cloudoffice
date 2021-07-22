aae52bf0fecef008c766a8cb111c638b4a220dd9
chadgeary <chad.geary@gmail.com>
2021-07-22 13:41:21 -0400

    recursive lightsail s3

--- lightsail/aws-s3.tf
+++ lightsail/aws-s3.tf
@@ -94,7 +94,7 @@ resource "aws_s3_bucket_public_access_block" "nc-bucket-pubaccessblock" {
 
 # s3 objects (playbook)
 resource "aws_s3_bucket_object" "nc-files" {
-  for_each       = fileset("../playbooks/", "*")
+  for_each       = fileset("../playbooks/", "**")
   bucket         = aws_s3_bucket.nc-bucket.id
   key            = "playbook/${each.value}"
   content_base64 = base64encode(file("${path.module}/../playbooks/${each.value}"))
